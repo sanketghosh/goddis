b911f6f3d045de9cf4fa10d37a703286c282b892
sanket <code.sanket13@gmail.com>
2024-06-09 01:45:09 +0530

    add: working on tcp server setup

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import "net"
 
-func main() {
-	fmt.Println("hello world")
+const defaultListenAddr = ":5000"
+
+type Config struct {
+	ListenAddr string
+}
+
+type Server struct {
+	Config
+	ln net.Listener
+}
+
+func NewServer(cfg Config) *Server {
+
+	/*
+		 if the length of listen address is 0 or not available
+		just assign the PORT number to default listen address PORT number
+	*/
+
+	if len(cfg.ListenAddr) == 0 {
+		cfg.ListenAddr = defaultListenAddr
+	}
+
+	return &Server{
+		Config: cfg,
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.ListenAddr)
+
+	if err != nil {
+		return err
+	}
+
+	s.ln = ln
+	return nil
 }
